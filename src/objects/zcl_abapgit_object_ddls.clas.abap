filak-sap/abapGit

commit 2f9e46421b7010812cff57ab0ff5a3c3547de13d

DDLS: ignore ACTFLAG and CHGFLAG
Changes to these components disrupt me for nothing.
I do not believe these components needs to be versioned.

src/objects/zcl_abapgit_object_ddls.clas.abap
@@ -250,7 +250,8 @@ CLASS ZCL_ABAPGIT_OBJECT_DDLS IMPLEMENTATION.
   METHOD zif_abapgit_object~serialize.
 
     DATA: lo_ddl  TYPE REF TO object,
-          lr_data TYPE REF TO data.
+          lr_data TYPE REF TO data,
+          lt_clr_comps TYPE STANDARD TABLE OF fieldname WITH DEFAULT KEY.
 
     FIELD-SYMBOLS: <lg_data>  TYPE any,
                    <lg_field> TYPE any.
@@ -274,15 +275,17 @@ CLASS ZCL_ABAPGIT_OBJECT_DDLS IMPLEMENTATION.
         zcx_abapgit_exception=>raise( 'DDLS error reading' ).
     ENDTRY.
 
-    ASSIGN COMPONENT 'AS4USER' OF STRUCTURE <lg_data> TO <lg_field>.
-    ASSERT sy-subrc = 0.
-    CLEAR <lg_field>.
-    ASSIGN COMPONENT 'AS4DATE' OF STRUCTURE <lg_data> TO <lg_field>.
-    ASSERT sy-subrc = 0.
-    CLEAR <lg_field>.
-    ASSIGN COMPONENT 'AS4TIME' OF STRUCTURE <lg_data> TO <lg_field>.
-    ASSERT sy-subrc = 0.
-    CLEAR <lg_field>.
+    APPEND 'AS4USER' TO lt_clr_comps.
+    APPEND 'AS4DATE' TO lt_clr_comps.
+    APPEND 'AS4TIME' TO lt_clr_comps.
+    APPEND 'ACTFLAG' TO lt_clr_comps.
+    APPEND 'CHGFLAG' TO lt_clr_comps.
+
+    LOOP AT lt_clr_comps ASSIGNING field-symbol(<lv_comp>).
+      ASSIGN COMPONENT <lv_comp> OF STRUCTURE <lg_data> TO <lg_field>.
+      ASSERT sy-subrc = 0.
+      CLEAR <lg_field>.
+    ENDLOOP.
 
     ASSIGN COMPONENT 'SOURCE' OF STRUCTURE <lg_data> TO <lg_field>.
     ASSERT sy-subrc = 0.

commit 1f56d203f8d751923c3d45b25a1e5c302103ac94

DDLS: fix a syntax error on ABAP <7.4
Introduced in 2f9e46421b7010812cff57ab0ff5a3c3547de13d
Reported as https://github.com/larshp/abapGit/commit/2f9e46421b7010812cff57ab0ff5a3c3547de13d#r28962636
Thank you, @testitorleaveit

src/objects/zcl_abapgit_object_ddls.clas.abap
@@ -254,7 +254,8 @@ CLASS ZCL_ABAPGIT_OBJECT_DDLS IMPLEMENTATION.
           lt_clr_comps TYPE STANDARD TABLE OF fieldname WITH DEFAULT KEY.
 
     FIELD-SYMBOLS: <lg_data>  TYPE any,
-                   <lg_field> TYPE any.
+                   <lg_field> TYPE any,
+                   <lv_comp>  TYPE string.
 
 
     CREATE DATA lr_data TYPE ('DDDDLSRCV').
@@ -281,7 +282,7 @@ CLASS ZCL_ABAPGIT_OBJECT_DDLS IMPLEMENTATION.
     APPEND 'ACTFLAG' TO lt_clr_comps.
     APPEND 'CHGFLAG' TO lt_clr_comps.
 
-    LOOP AT lt_clr_comps ASSIGNING field-symbol(<lv_comp>).
+    LOOP AT lt_clr_comps ASSIGNING <lv_comp>.
       ASSIGN COMPONENT <lv_comp> OF STRUCTURE <lg_data> TO <lg_field>.
       ASSERT sy-subrc = 0.
       CLEAR <lg_field>.